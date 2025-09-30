14a567fd473cd8bbb5bcba58a735ce9214bde216
tecmalik <abdulmalikojo2@gmail.com>
2025-09-30 11:56:15 +0100

    creating package

---
+++ candidatePark/src/main/java/com/example/candidatepark/CandidateParkApplication.java
@@ -0,0 +1,13 @@
+package com.example.candidatepark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CandidateParkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CandidateParkApplication.class, args);
+    }
+
+}
---
+++ candidatePark/src/test/java/com/example/candidatepark/CandidateParkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.candidatepark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CandidateParkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
